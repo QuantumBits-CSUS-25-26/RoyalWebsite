e9d4e1c85bc5ef841d92f3b3d12955d2b34d68d3
nuhret <gould.trevor.i@gmail.com>
2025-10-19 21:07:25 -0700

    RW-80: initial database connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'testdb',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        #change user/pass as needed
+        'USER': 'root',
+        'PASSWORD': 'Root',
     }
 }
 
