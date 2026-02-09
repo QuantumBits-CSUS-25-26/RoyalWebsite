e2f2891da7902c3f630c0c2b03326387352a1cab
Erik <erikcontreras@csus.edu>
2026-02-08 21:30:40 -0800

    RW-63
    Step 4 of appointment page: contact information

--- frontend/src/Pages/Appointments.js
+++ frontend/src/Pages/Appointments.js
@@ -1,7 +1,36 @@
+import React, {useState} from 'react';
+import ContactInfoS4 from "../Components/ContactInfoS4";
 const Appointments = () => {
+  const [contactInfo, setContactInfo] = useState({
+    fname:'',
+    lname:'',
+    email:'',
+    phone:'',
+    notifPref:{
+      email:false,
+      text:false
+    }
+  });
+  function handleFieldChange(e){
+    const {name, value} = e.target;
+    setContactInfo(prev=> ({ ...prev, [name]: value}));
+  }
+  function handleNotifChange(e){
+    const {name, checked} = e.target;
+    setContactInfo(prev=> ({...prev, notifPref: {...prev.notifPref, [name]: checked}}))
+  };
+  function handleSubmit(e){
+    console.log("Submitting: ", contactInfo);
+  }
   return (
     <div>
       Appointments
+      <ContactInfoS4 
+        contactInfo={contactInfo}
+        onFieldChange={handleFieldChange}
+        onNotifChange={handleNotifChange}
+        onSubmit={handleSubmit}
+        />
     </div>
   )
 }
